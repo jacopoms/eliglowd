9f4962b8fdef3ed4485a505c2056f8dec02cfd62
Jacopo Ghisolfi <jacopo.ghisolfi@marleyspoon.com>
2022-07-14 14:30:45 +0200

    added new function for search articles based on a query string

--- lib/eliglowd/guadian_api/guardian_api/client.ex
+++ lib/eliglowd/guadian_api/guardian_api/client.ex
@@ -4,6 +4,19 @@ defmodule Eliglowd.GuardianApi.Client do
   @base_url "http://content.guardianapis.com/"
   @api_key Application.get_env(:eliglowd, :api_key)
 
+
+  def fetch_articles_for(query) do
+    case get("search?q=" <> query) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        %{"results" => results } = elem(body, 1)
+        results
+      {:ok, %HTTPoison.Response{status_code: 404 }} ->
+        IO.puts "Not Found"
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.puts "Error" <> reason
+    end
+  end
+
   def process_request_url(url) do
     @base_url <> url
   end
@@ -19,6 +32,7 @@ defmodule Eliglowd.GuardianApi.Client do
     |> Jason.decode!
     |> Map.fetch("response")
   end
+
 
   defp api_key do
     [ "Api-Key": @api_key]
